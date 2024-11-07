da8da804237490546d687f7275bf928985508752
Kumar Abhinav <kabhinav.iitg@gmail.com>
2024-11-07 15:18:42 +0530

    aa

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-export const Dashboard:React.FC = () => {
+export const Dashboard: React.FC = () => {
   return (
     <div>
-        <div>
-            <h1>Bayer Health</h1>
-            <nav>Dashboard</nav>
-            <nav>Patient List</nav>
-            <nav>Appointments</nav>
-            <nav>Messages</nav>
-            <nav>Logout</nav>
-        </div>
-        <Outlet/>
+      <div className="bg-blue-600 text-white font-bold p-4 w-52 h-screen">
+        <h1 className="py-4">Bayer Health</h1>
+        <ul className="text-sm">
+          <li>Dashboard</li>
+          <li>Patient List</li>
+          <li>Appointments</li>
+          <li>Messages</li>
+          <li>Logout</li>
+        </ul>
+      </div>
+      <Outlet />
     </div>
-  )
-}
+  );
+};
