e338228d116c56ef03c41bd96cf9ceccce43dc5d
Kumar Abhinav <kabhinav.iitg@gmail.com>
2024-11-07 14:39:59 +0530

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,45 @@
 const Home = () => {
-    return (
-        <div>
-            <div className="bg-blue-600 text-white">Buyer Healthcare</div>
-            
+  return (
+    <div>
+      <div className="bg-blue-600 text-white py-8 text-center font-bold text-xl">
+        Buyer Healthcare
+      </div>
+      <div className=" bg-green-600 text-white flex justify-center gap-4 font-bold text-sm py-2">
+        <a href="#">Home</a>
+        <a href="#">Health Topics</a>
+        <a href="#">Resources</a>
+        <a href="#">About Us</a>
+        <a href="#">Contact</a>
+      </div>
+      <div className="bg-blue-600 text-white py-8 text-center font-bold text-xl">
+        <div className="text-2xl">Your Health, Our Priority</div>
+        <div className="text-xs py-4">
+          Explore the latest health information and resources from Bayer
+          Healthcare
         </div>
-    );
+      </div>
+      <div className="bg-gray-200 pt-16 px-10">
+        <h2 className="font-bold pb-2">Featured Health Topics</h2>
+        <div className="flex gap-4 pb-8">
+            <div className="bg-white w-64 p-4">
+                <h3 className="font-bold pb-2">COVID 19 Updates</h3>
+                <div className="text-sm pb-2">Stay informed about the latest COVID 19 guidelines and vaccination information</div>
+                <button className="text-sm bg-green-600 text-white px-2 py-1">Learn More</button>
+            </div>
+            <div className="bg-white w-64 p-4">
+                <h3 className="font-bold pb-2">COVID 19 Updates</h3>
+                <div className="text-sm pb-2">Stay informed about the latest COVID 19 guidelines and vaccination information</div>
+                <button className="text-sm bg-green-600 text-white px-2 py-1">Learn More</button>
+            </div>
+            <div className="bg-white w-64 p-4">
+                <h3 className="font-bold pb-2">COVID 19 Updates</h3>
+                <div className="text-sm pb-2">Stay informed about the latest COVID 19 guidelines and vaccination information</div>
+                <button className="text-sm bg-green-600 text-white px-2 py-1">Learn More</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
